68b645b585c9ef47f2535c18f55af230ab8a889c
tygedavis <tyge.davis@gmail.com>
2019-12-20 09:45:33 -0700

    Login page will render to bubblepage with username and password 'lambda' and 'school'

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -11,6 +11,7 @@ const BubblePage = () => {
 
   return (
     <>
+      <h1>The Bubble Page is Rendering</h1>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
